275edff27f70e5e2e257b4f87d5e66b33ea34017
Joseph Daza <josephdazz@hotmail.com>
2024-05-08 13:20:34 -0400

    vite.config.js addded

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Spinner from './components/spinner'
+import NavBar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
      <Spinner/>
+     <NavBar/>
     </>
   )
 }
