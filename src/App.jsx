49f3398aef47dbe2439aad44d6e3acc9e2d25484
Joseph Daza <josephdazz@hotmail.com>
2024-05-08 13:43:21 -0400

    scss moved to assets

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Spinner from './components/spinner'
 import NavBar from './components/navbar'
+import './assets/scss/bootstrap.scss'
 
 function App() {
   const [count, setCount] = useState(0)
