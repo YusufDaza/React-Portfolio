fc27c91977053a64d8e7e4a060b5ee47f3ef2fda
Joseph Daza <josephdazz@hotmail.com>
2024-05-08 21:19:39 -0400

    portfolio.jsx, resume.jsx, contact.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,38 @@ import './App.css'
 import Spinner from './components/spinner'
 import NavBar from './components/navbar'
 // import './assets/scss/bootstrap.scss'
+import Home from './pages/home'
+import Portfolio from './pages/portfolio'
+import About from './pages/about'
+import Contact from './pages/contact'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    console.log(currentPage)
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <>
-     <NavBar/>
+     <NavBar currentPage={currentPage} handlePageChange={handlePageChange}/>
+     {renderPage()}
     </>
   )
 }
