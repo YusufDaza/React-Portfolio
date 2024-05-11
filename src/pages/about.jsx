cae68fd1c8ed755b70d0a62ad2f216fab4da1f15
Joseph Daza <josephdazz@hotmail.com>
2024-05-10 22:31:38 -0400

    index.jsx updated

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,11 +6,10 @@ function About(){
                     <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                         <h5 class="text-primary-gradient fw-medium">About</h5>
                         <h1 class="mb-4">Joseph "Yusuf" Daza</h1>
-                        <p class="mb-4">Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit clita duo justo eirmod magna dolore erat amet</p>
+                        <p class="mb-4">Yusuf is a junior coder with a deep passion for both coding and visual arts. Combining their technical skills with a keen eye for aesthetics. With a solid foundation in programming languages like HTML, CSS and JavaScript, Yusuf enjoys tackling coding challenges and developing software solutions. Outside coding, Yusuf spends time honing his skills in various visual arts mediums, from digital illustration and graphic design. He believe that the fusion of coding and visual arts opens doors to endless possibilities.</p>
                         <div class="row g-4 mb-4">
                             <div class="col-sm-6 wow fadeIn" data-wow-delay="0.5s">
                                 <div class="d-flex">
-                                    <i class="fa fa-cogs fa-2x text-primary-gradient flex-shrink-0 mt-1"></i>
                                     <div class="ms-3">                                       
                                     </div>
                                 </div>
